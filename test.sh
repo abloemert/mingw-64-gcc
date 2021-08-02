144b88b249c5d3e4c714ad37cf13bb7d89792736
Arnold Bloemert <abloemert@gmail.com>
2021-08-02 22:02:08 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,10 +8,13 @@ set -e
 # print all commands
 set -x
 
+wget -q http://ftpmirror.gnu.org/gcc/gcc-8.4.0/gcc-8.4.0.tar.gz
+tar xzf gcc-8.4.0.tar.gz
+
 wget -q https://nav.dl.sourceforge.net/project/mingw-w64/mingw-w64/mingw-w64-release/mingw-w64-v9.0.0.tar.bz2
 tar xjf mingw-w64-v9.0.0.tar.bz2
 
-mkdir build-mingw-w64
+mkdir build-mingw-w64 dest
 cd build-mingw-w64
 
 ../mingw-w64-v9.0.0/configure \
@@ -19,8 +22,9 @@ cd build-mingw-w64
   --host=x86_64-w64-mingw32 \
   --target=x86_64-w64-mingw32 \
   --disable-lib32 \
-  --prefix=/c/foobar \
-  --with-sysroot=/c/foobar \
+  --prefix=../dest/x86_64-w64-mingw32 \
+  --with-sysroot=../dest/x86_64-w64-mingw32 \
   --enable-wildcard \
   --with-libraries=winpthreads \
-  --disable-shared
+  --disable-shared \
+  || cat config.log
