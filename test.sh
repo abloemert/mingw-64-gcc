be7161af030f62d273209bd7a95e357cfb315474
Arnold Bloemert <abloemert@gmail.com>
2021-08-06 21:22:10 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -21,6 +21,10 @@ MAKE="make -s -j${NUMBER_OF_PROCESSORS} -O"
 export CFLAGS="-s -O3 -Wno-expansion-to-defined -pipe"
 export CXXFLAGS="${CFLAGS}"
 
+TARGET="x86_64-w64-mingw32"
+BUILD=$TARGET
+HOST=$TARGET
+
 echo "## Download mingw-w64 sources"
 wget -q https://nav.dl.sourceforge.net/project/mingw-w64/mingw-w64/mingw-w64-release/mingw-w64-v6.0.0.tar.bz2
 tar xjf mingw-w64-v6.0.0.tar.bz2
@@ -30,12 +34,12 @@ cd ${WORKSPACE}/build-mingw-w64
 
 echo "## Configure mingw-w64"
 ../mingw-w64-v6.0.0/configure \
-  --build=x86_64-w64-mingw32 \
-  --host=x86_64-w64-mingw32 \
-  --target=x86_64-w64-mingw32 \
+  --build=${BUILD} \
+  --host=${HOST} \
+  --target=${TARGET} \
   --disable-lib32 \
-  --prefix=${PREFIX}/x86_64-w64-mingw32 \
-  --with-sysroot=${PREFIX}/x86_64-w64-mingw32 \
+  --prefix=${PREFIX}/${TARGET} \
+  --with-sysroot=${PREFIX}/${TARGET}/sysroot \
   --enable-wildcard \
   --with-libraries=winpthreads \
   --disable-shared
@@ -69,10 +73,10 @@ mv mpc-1.1.0 src/mpc
 mv isl-0.18 src/isl
 
 echo "## Prepare to build gcc"
-cp -r ${PREFIX}/x86_64-w64-mingw32/lib ${PREFIX}/x86_64-w64-mingw32/lib64
-cp -r ${PREFIX}/x86_64-w64-mingw32 ${PREFIX}/mingw
+cp -r ${PREFIX}/${TARGET}/lib ${PREFIX}/${TARGET}/lib64
+cp -r ${PREFIX}/${TARGET} ${PREFIX}/mingw
 mkdir -p src/gcc/winsup/mingw
-cp -r ${PREFIX}/x86_64-w64-mingw32/include src/gcc/winsup/mingw/include
+cp -r ${PREFIX}/${TARGET}/include src/gcc/winsup/mingw/include
 
 mkdir build
 cd build
@@ -82,9 +86,9 @@ echo "## Configure gcc"
   --prefix=${PREFIX} \
   --with-slibdir="${PREFIX}/lib" \
   --libdir="${PREFIX}/lib" \
-  --build=x86_64-w64-mingw32 \
-  --host=x86_64-w64-mingw32 \
-  --target=x86_64-w64-mingw32 \
+  --build=${BUILD} \
+  --host=${HOST} \
+  --target=${TARGET} \
   --enable-default-pie \
   --enable-languages=c,c++,fortran,objc,obj-c++ \
   --enable-__cxa_atexit \
@@ -100,20 +104,15 @@ echo "## Configure gcc"
   --enable-plugin \
   --enable-gold \
   --disable-nls \
-  --disable-bootstrap \
   --disable-multilib \
   --enable-long-long \
   --enable-default-pie \
-  --with-sysroot=${PREFIX} \
+  --with-sysroot=${PREFIX}/${TARGET}/sysroot
+  --with-gxx-include-dir="${PREFIX}/${TARGET}/include/c++/8.4.0"
   --disable-libstdcxx-pch \
   --disable-libstdcxx-verbose \
   --disable-win32-registry \
   --with-tune=haswell
-
-#  --with-sysroot=${PREFIX}/${TARGET}/sysroot
-#  --with-build-sysroot=${BUILD_PREFIX}/${TARGET}/sysroot
-#  --with-gxx-include-dir="${PREFIX}/${TARGET}/include/c++/${gcc_version}"
-
 
 echo "## Build gcc"
 ${MAKE} "CFLAGS=-g0 -O3" "CXXFLAGS=-g0 -O3" "CFLAGS_FOR_TARGET=-g0 -O3" "CXXFLAGS_FOR_TARGET=-g0 -O3" "BOOT_CFLAGS=-g0 -O3" "BOOT_CXXFLAGS=-g0 -O3"
