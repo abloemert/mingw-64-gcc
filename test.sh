1cea229533a06bb42e4cc50e7e34dde0782c65bd
Arnold Bloemert <abloemert@gmail.com>
2021-08-29 23:52:17 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -20,6 +20,7 @@ MAKE="make -s -j${NUMBER_OF_PROCESSORS} -O"
 
 export CFLAGS="-s -O3 -Wno-expansion-to-defined -pipe"
 export CXXFLAGS="${CFLAGS}"
+export LDFLAGS="-Wl,-no-undefined" # fix shared builds
 
 TARGET="x86_64-w64-mingw32"
 BUILD=$TARGET
